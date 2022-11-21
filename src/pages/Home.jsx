0ef1a790d582514aedf37f025a1255e1068bafcd
skyhaf <hafiznasution2002@gmail.com>
2022-11-22 03:22:19 +0700

    done ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,17 @@ import down from "../static/svg/down.svg"
 import { Link } from "react-router-dom"
 import sportfamilyamico from "../static/svg/sport-family-amico.svg"
 import BadmintonAmico from "../static/svg/Badminton-amico.svg"
+import locationWhite from "../static/svg/locationWhite.svg"
+import harga from "../static/svg/harga.png"
 
 import { ReactComponent as MyLogo } from "../static/svg/card.svg"
 import api from "../helpers/api"
+import { useState } from "react"
 
 function Home() {
   const navigate = useNavigate()
   const role = "pengguna"
+  const [isLapanganExist, setIsLapanganExist] = useState(false)
 
   // useEffect(() => {
   //   async function getData() {
@@ -38,7 +42,7 @@ function Home() {
 
   return (
     <>
-      {role === "pemilik" ? (
+      {role === "pengguna" ? (
         <>
           <div className="h-full flex flex-col overflow-y-hidden relative">
             {/* header */}
@@ -87,48 +91,108 @@ function Home() {
       ) : (
         <>
           {" "}
-          <div className="h-full flex flex-col overflow-y-hidden relative px-6">
-            {/* header */}
-            {/* <div className="bg-white top-0 pt-4">
-              <img src={gorkusimple} alt="" />
-            </div> */}
-            <div className="flex bg-white justify-center top-0 pt-4">
-              <img src={GorKu} alt="" />
-            </div>
-
-            {/* konten */}
-            <div className="flex flex-col flex-grow max-h-screen absolute inset-x-0 bottom-[50px] justify-center items-center space-y-4 z-10">
-              <div className="w-5/6 text-center font-black text-[16px]">
-                Halo,{" "}
-                <span className="text-blue-primary"> Andre Silalahi! </span>
-                <br />
-                Selamat bergabung di{" "}
-                <span className="text-blue-primary"> GorKu </span>
-              </div>
-
-              <div>
-                <img src={BadmintonAmico} alt="" className="h-40" />
-              </div>
-
-              <div className="w-5/6 text-center font-black text-[16px]">
-                Yuk <span className="text-blue-primary"> daftarkan </span>
-                lapangan olahraga yang kamu punya di bawah sini
-              </div>
-
-              <div className="flex flex-col space-y-4">
-                <div className="flex justify-center">
-                  <img src={down} alt="" className="animate-bounce h-8 w-8" />
+          {isLapanganExist ? (
+            <>
+              <div className="h-full flex flex-col overflow-y-hidden relative">
+                {/* header */}
+                <div className="flex bg-white justify-center top-0 pt-4">
+                  <img src={GorKu} alt="" />
                 </div>
 
-                <Link
-                  to="/tambah-lapangan"
-                  className="font-black text-white bg-blue-primary px-6 py-3 text-base rounded-md"
-                >
-                  Tambahkan Lapangan Baru
-                </Link>
+                {/* konten */}
+                <div className="h-96 m-8 space-y-3">
+                  <h1 className="underline text-blue-primary text-lg font-semibold">
+                    Lapangan Saat Ini
+                  </h1>
+                  <div className="h-full w-full space-y-3 border-2 border-blue-primary bg-blue-primary rounded-xl text-white p-4">
+                    <div className="h-36 flex justify-center">
+                      <img src={BadmintonAmico} className="h-36" alt="" />
+                    </div>
+                    <div className="">
+                      <h1 className="text-base font-semibold">Nama Lapangan</h1>
+                      <p>Lapangan Badminton</p>
+                    </div>
+
+                    <div>
+                      <h1 className="text-[10px]">
+                        {" "}
+                        <span className="font-semibold">Alamat: </span> Jalan
+                        H.Mandor Salim No. 3A 6 1, RT.2, Srengseng, Kembangan,
+                        West Jakarta City, Jakarta 11630
+                      </h1>
+
+                      <h1 className="text-[10px]">
+                        {" "}
+                        <span className="font-semibold">Harga sewa: </span> Rp
+                        60.000,00/jam
+                      </h1>
+
+                      <h1 className="text-[10px]">
+                        {" "}
+                        <span className="font-semibold">Jam buka: </span> 08.00
+                        - 10.00
+                      </h1>
+                    </div>
+
+                    <div className="pt-3">
+                      <Link to="/riwayat-lapangan" className="rounded-lg h-8 flex justify-center items-center bg-blue-secondary font-black text-sm text-blue-primary">
+                        Lihat Riwayat Pesanan Lapangan
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </>
+          ) : (
+            <>
+              <div className="h-full flex flex-col overflow-y-hidden relative px-6">
+                {/* header */}
+                {/* <div className="bg-white top-0 pt-4">
+              <img src={gorkusimple} alt="" />
+            </div> */}
+                <div className="flex bg-white justify-center top-0 pt-4">
+                  <img src={GorKu} alt="" />
+                </div>
+
+                {/* konten */}
+                <div className="flex flex-col flex-grow max-h-screen absolute inset-x-0 bottom-[50px] justify-center items-center space-y-4 z-10">
+                  <div className="w-5/6 text-center font-black text-[16px]">
+                    Halo,{" "}
+                    <span className="text-blue-primary"> Andre Silalahi! </span>
+                    <br />
+                    Selamat bergabung di{" "}
+                    <span className="text-blue-primary"> GorKu </span>
+                  </div>
+
+                  <div>
+                    <img src={BadmintonAmico} alt="" className="h-40" />
+                  </div>
+
+                  <div className="w-5/6 text-center font-black text-[16px]">
+                    Yuk <span className="text-blue-primary"> daftarkan </span>
+                    lapangan olahraga yang kamu punya di bawah sini
+                  </div>
+
+                  <div className="flex flex-col space-y-4">
+                    <div className="flex justify-center">
+                      <img
+                        src={down}
+                        alt=""
+                        className="animate-bounce h-8 w-8"
+                      />
+                    </div>
+
+                    <Link
+                      to="/tambah-lapangan"
+                      className="font-black text-white bg-blue-primary px-6 py-3 text-base rounded-md"
+                    >
+                      Tambahkan Lapangan Baru
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </>
       )}
     </>
